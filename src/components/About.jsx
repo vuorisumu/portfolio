c3e6c30a159ae2cec6afd869bacd4870eef8392c
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-23 20:11:07 +0300

    feat: add info to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,41 @@ import { useTranslation } from "react-i18next";
 
 function About() {
   const { t } = useTranslation();
+  const desc = t("aboutDesc", { returnObjects: true });
+  const strengths = t("strengths", { returnObjects: true });
+  const skills = t("skills", { returnObjects: true });
 
   return (
     <article>
-      <h2>{t("about")}</h2>
-      <p>{t("aboutDesc")}</p>
+      <section>
+        <h2>{t("about")}</h2>
+        {desc.map((d, index) => (
+          <p key={`desc${index}`}>{d}</p>
+        ))}
+      </section>
+
+      <section>
+        <h2>{t("strengthsTitle")}</h2>
+        <ul>
+          {strengths.map((strength, index) => (
+            <li key={`str${index}`}>
+              <h3>{strength.name}</h3>
+              <p>{strength.description}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2>{t("skillsTitle")}</h2>
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={`skill${index}`}>
+              <p>{skill}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
     </article>
   );
 }
