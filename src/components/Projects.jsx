182e6e81220e9b204e51b2fb273bd6a9e921a039
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-23 18:38:55 +0300

    feat: show more from one selected project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function Projects() {
   const { t } = useTranslation();
   const projects = t("projects", { returnObjects: true });
+  const [selected, setSelected] = useState({});
+
+  useEffect(() => {
+    setSelected(projects[0]);
+  }, [projects]);
 
   return (
     <article>
       <h2>{t("projectTitle")}</h2>
 
-      <ul>
+      <div id="projectDisplay">
+        <h3>{selected.name}</h3>
+        <p>{selected.description}</p>
+      </div>
+
+      <ul className="projects">
         {projects.map((project, index) => (
           <li key={`project${index}`}>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
+            <button onClick={() => setSelected(project)}>
+              <h3>{project.name}</h3>
+            </button>
           </li>
         ))}
       </ul>
