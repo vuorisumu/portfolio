578959a8f87cb4d36104df902a8c772f9102c5e0
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-23 20:46:15 +0300

    feat: add styles to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,35 +8,35 @@ function About() {
 
   return (
     <article>
-      <section>
-        <h2>{t("about")}</h2>
-        {desc.map((d, index) => (
-          <p key={`desc${index}`}>{d}</p>
-        ))}
-      </section>
-
-      <section>
-        <h2>{t("strengthsTitle")}</h2>
-        <ul>
-          {strengths.map((strength, index) => (
-            <li key={`str${index}`}>
-              <h3>{strength.name}</h3>
-              <p>{strength.description}</p>
-            </li>
+      <div id="about">
+        <section>
+          <h2>{t("about")}</h2>
+          {desc.map((d, index) => (
+            <p key={`desc${index}`}>{d}</p>
           ))}
-        </ul>
-      </section>
+        </section>
 
-      <section>
-        <h2>{t("skillsTitle")}</h2>
-        <ul>
-          {skills.map((skill, index) => (
-            <li key={`skill${index}`}>
-              <p>{skill}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
+        <section>
+          <h2>{t("strengthsTitle")}</h2>
+          <ul>
+            {strengths.map((strength, index) => (
+              <li key={`str${index}`}>
+                <h3>{strength.name}</h3>
+                <p>{strength.description}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section>
+          <h2>{t("skillsTitle")}</h2>
+          <ul className="skills">
+            {skills.map((skill, index) => (
+              <li key={`skill${index}`}>{skill}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </article>
   );
 }
