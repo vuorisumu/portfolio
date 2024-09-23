b47d855df881c28f923300de2b0497964ba2782f
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-23 22:29:22 +0300

    feat: add styles to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import { Suspense } from "react";
+import { HashRouter as Router } from "react-router-dom";
 import GeneralInfo from "./components/GeneralInfo";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Cv from "./components/Cv";
 import Contact from "./components/Contact";
 import Nav from "./components/Nav";
-import Language from "./components/Language";
-import Theme from "./components/Theme";
 
 function App() {
   return (
-    <Suspense fallback="loading">
-      <div id="container">
-        <nav>
-          <Nav />
-        </nav>
+    <Router>
+      <Suspense fallback="loading">
+        <div id="container">
+          <nav>
+            <Nav />
+          </nav>
 
-        <div id="content">
-          <Language />
-          <Theme />
-          <main>
-            <GeneralInfo />
-            <About />
-            <Projects />
-            <Cv />
-            <Contact />
-          </main>
+          <div id="content">
+            <main>
+              <GeneralInfo />
+              <About />
+              <Projects />
+              <Cv />
+              <Contact />
+            </main>
+          </div>
+          <footer>
+            <p>Sumu Vuori</p>
+          </footer>
         </div>
-        <footer>
-          <p>Sumu Vuori</p>
-        </footer>
-      </div>
-    </Suspense>
+      </Suspense>
+    </Router>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,36 @@
+import { HashLink as Link } from "react-router-hash-link";
+import Language from "./Language";
+import Theme from "./Theme";
+import { useTranslation } from "react-i18next";
+
 function Nav() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <p>Nav</p>
+      <ul>
+        <li>
+          <Link to="/#about">{t("about")}</Link>
+        </li>
+        <li>
+          <Link to="/#projects">{t("projectTitle")}</Link>
+        </li>
+        <li>
+          <Link to="/#experience">{t("experienceTitle")}</Link>
+        </li>
+        <li>
+          <Link to="/#contact">{t("contact")}</Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Language />
+        </li>
+        <li>
+          <Theme />
+        </li>
+      </ul>
     </div>
   );
 }
