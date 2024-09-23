c0c66ab1b78afe15b1f7cedc466fc4f1bf317cda
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-23 17:33:16 +0300

    feat: add experience placeholders

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,10 +1,26 @@
-import Experience from "./Experience";
+import { useTranslation } from "react-i18next";
 
 function Cv() {
+  const { t } = useTranslation();
+  const experience = t("experience", { returnObjects: true });
+
   return (
     <article>
-      <Experience />
-      <Experience />
+      <ul>
+        {experience.map((expField) => (
+          <li key={`expField${expField.name}`}>
+            <h2>{expField.name}</h2>
+            <ul>
+              {expField.contents.map((exp, index) => (
+                <li key={`${expField.name}${index}`}>
+                  <h3>{exp.name}</h3>
+                  <p>{exp.description}</p>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </article>
   );
 }
--- src/components/Experience.jsx
+++
@@ -1,9 +0,0 @@
-function Experience() {
-  return (
-    <section>
-      <h2>Experience</h2>
-    </section>
-  );
-}
-
-export default Experience;
