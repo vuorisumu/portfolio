d232664014e96c42593cf7591f0e0fcdbf093cbc
Sumu Vuori <vuori.sumu@gmail.com>
2024-09-24 17:45:37 +0300

    feat: add contact information and socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,26 @@ function Contact() {
   return (
     <article id="contact">
       <h2>{t("contact")}</h2>
+      <p>
+        <span className="material-symbols-outlined">mail</span>
+        vuori.sumu[at]gmail.com
+      </p>
+      <p>
+        <span className="material-symbols-outlined">call</span> 0449884833
+      </p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/sumu-vuori-515aa92a1/"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/vuorisumu" target="_blank">
+          GitHub
+        </a>
+      </p>
     </article>
   );
 }
